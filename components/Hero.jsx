f3922086eddae9aaacc8269d13b0603e4b6298b8
Zinat411 <zinatabohgool@gmail.com>
2024-09-29 21:10:51 +0300

    changing the home page gui

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -1,25 +1,17 @@
 import React from 'react';
-import Link from 'next/link';
-
 import styles from '@/scss/components/Hero.module.scss';
 
 const Hero = () => {
-  return (
-    <div className={styles.heroContainer}>
-      <div className={styles.title}>
-        <h1 className={styles.title__primary}>MOONQUAKE VISUALIZED</h1>
-        <h2 className={styles.title__secondary}>VISUALIZING THE MOON SINCE 1969</h2>
-      </div>
-      <div className={styles.links}>
-        <Link
-          href='/simulation'
-          className={styles.links__link}
-        >
-          &lt; Visit Moon &gt;
-        </Link>
-      </div>
-    </div>
-  );
+    return (
+        <div className={styles.hero}>
+            <h1 className={styles.title}>Seismic Quakes</h1>
+            <div className={styles.buttonContainer}>
+                <button className={styles.missionButton}>Explore the moon</button>
+                <button className={styles.missionButton}>Explore Mars</button>
+            </div>
+            <p className={styles.description}>Choose your celestial destination and embark on an interplanetary adventure.</p>
+        </div>
+    );
 };
 
 export default Hero;
--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -4,22 +4,17 @@ import Link from 'next/link';
 import styles from '@/scss/components/Navbar.module.scss';
 
 const Navbar = () => {
-  return (
-    <nav className={styles.navbarContainer}>
-      <Link
-        className={styles.brand}
-        href='/'
-      >
-        Home
-      </Link>
-      <Link
-        className={styles.link}
-        href='/about'
-      >
-        About
-      </Link>
-    </nav>
-  );
+    return (
+        <nav className={styles.navbar}>
+            <div className={styles.logo}>Fourier Foreseers</div>
+            <ul className={styles.navLinks}>
+                <li><a href="#home">HOME</a></li>
+                <li><Link href="/mission">MISSION</Link></li>
+                <li><a href="#team">TEAM</a></li>
+                <li><a href="#contact">CONTACT</a></li>
+            </ul>
+        </nav>
+    );
 };
 
 export default Navbar;
