e3bddbb2eb0d68f961c53eed110dded2eeffef6e
Zinat411 <zinatabohgool@gmail.com>
2024-10-03 10:44:59 +0300

    adding the mission page

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,13 @@ module.exports = {
   content: [
     './app/**/*.{js,ts,jsx,tsx}',  // Include all files in the pages directory
     './components/**/*.{js,ts,jsx,tsx}',  // Include all files in the components directory
-
   ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'space-image': "url('/assets/mission-background.jpg')",
+      }
+    },
   },
   plugins: [],
 }
